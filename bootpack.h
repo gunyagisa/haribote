39cf1f5ad4e17be3d310d48c30a195a347046ec4
gunyagisa <gunyagisa@gmail.com>
2019-12-09 11:00:57 +0900

    defineを追加、eflagsを読み込む関数のプロトタイプ宣言を追加

--- bootpack.h
+++ bootpack.h
@@ -3,6 +3,9 @@
 
 #define BOOTINFO_ADDR 	0x00000ff0
 
+#define EFLAGS_AC_BIT           0x00040000
+#define CR0_CACHE_DISABLE       0x60000000
+
 #include "fifo.h"
 
 // nasmfunc.asm
@@ -10,7 +13,7 @@ extern void io_hlt(void);
 extern void io_cli(void), io_sti(void), io_stihlt(void);
 extern void io_out8(int port, int data), io_out16(int port, int data), io_out32(int port, int data);
 extern int io_load_eflags(void), io_in8(int port), io_in16(int port), io_in32(int port);
-extern void io_store_eflags(int eflags);
+extern void io_store_eflags(int eflags), load_cr0(), store_cr0(int cr0);
 extern void load_gdtr(int limit, int addr);
 extern void load_idtr(int limit, int addr);
 extern void inthandler21_asm(void);
