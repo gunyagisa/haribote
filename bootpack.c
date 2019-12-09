c653b17868b32f68dc5994d14138e392fe71d94c
gunyagisa <gunyagisa@gmail.com>
2019-12-09 10:59:55 +0900

    メモリの容量を確認できるようになった

--- bootpack.c
+++ bootpack.c
@@ -7,6 +7,41 @@
 #include "keyboard.h"
 #include "mysprintf.c"
 
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+    char flg486 = 0;
+    unsigned int eflg, cr0, i;
+
+    // check 386 or 486 later
+    eflg = io_load_eflags();
+    eflg |= EFLAGS_AC_BIT;
+    io_store_eflags(eflg);
+    eflg = io_load_eflags();
+    if ((eflg & EFLAGS_AC_BIT) != 0) {
+        flg486 = 1;
+    }
+
+    eflg &= ~EFLAGS_AC_BIT;
+    io_store_eflags();
+
+    if (flg486 != 0) {
+        cr0 = load_cr0();
+        cr0 |= CR0_CACHE_DISABLE;
+        store_cr0(cr0);
+    }
+
+    i = memtest_sub(start, end);
+
+    if (flg486 != 0) {
+        cr0 = load_cr0;
+        cr0 &= ~CR0_CACHE_DISABLE;
+        store_cr0(cr0);
+    }
+
+    return i;
+}
+
+
 void HariMain(void) {
     BOOTINFO *binfo = (BOOTINFO *) BOOTINFO_ADDR;
     char *mouse, s[40], keybuf[32], mousebuf[128];
