a052f3e87e6f479721cfab5011eacce20c4c884d
gunyagisa <gunyagisa@gmail.com>
2019-12-12 16:01:33 +0900

    load_cr0のextern宣言を修正

--- bootpack.h
+++ bootpack.h
@@ -12,8 +12,8 @@
 extern void io_hlt(void);
 extern void io_cli(void), io_sti(void), io_stihlt(void);
 extern void io_out8(int port, int data), io_out16(int port, int data), io_out32(int port, int data);
-extern int io_load_eflags(void), io_in8(int port), io_in16(int port), io_in32(int port);
-extern void io_store_eflags(int eflags), load_cr0(), store_cr0(int cr0);
+extern int io_load_eflags(void), io_in8(int port), io_in16(int port), io_in32(int port),load_cr0();
+extern void io_store_eflags(int eflags), store_cr0(int cr0);
 extern void load_gdtr(int limit, int addr);
 extern void load_idtr(int limit, int addr);
 extern void inthandler21_asm(void);
