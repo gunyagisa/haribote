3222f31c19958121bc23a0d0c8f212ec113c183d
gunyagisa <gunyagisa@gmail.com>
2020-04-14 23:59:01 +0900

    change window focus by typing TAB

--- src/bootpack.c
+++ src/bootpack.c
@@ -10,8 +10,23 @@
 #include "dsctbl.h"
 
 void console_task(struct SHEET *);
+void make_wtitle8(unsigned char *, int, char *, char);
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
+{
+  boxfill8(buf, xsize, COL8_C6C6C6, 0,                 0, xsize - 1, 0);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1,                 1, xsize - 2, 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 0,                 0, 0, ysize - 1);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1,                 1, 1, ysize - 2);
+  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+  boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize -1);
+  make_wtitle8(buf, xsize, title, act);
+}
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
 {
   static char closebtn[14][16] = {
     "00000000000000$@",
@@ -38,19 +53,8 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
     tc = COL8_C6C6C6;
     tbc = COL8_848484;
   }
-
-  boxfill8(buf, xsize, COL8_C6C6C6, 0,                 0, xsize - 1, 0);
-  boxfill8(buf, xsize, COL8_FFFFFF, 1,                 1, xsize - 2, 1);
-  boxfill8(buf, xsize, COL8_C6C6C6, 0,                 0, 0, ysize - 1);
-  boxfill8(buf, xsize, COL8_FFFFFF, 1,                 1, 1, ysize - 2);
-  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
-  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
-  boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
-  boxfill8(buf, xsize, tbc,         3, 3, xsize - 4, 20);
-  boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
-  boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize -1);
-  str_renderer8(buf, xsize, tc, 24, 4, title);
-
+  boxfill8(buf, xsize, tbc, 3, 3,xsize - 4, 20);
+  str_renderer8(buf, xsize, 24, 4, tbc, title);
   for ( int y = 0;y < 14;++y) {
     for (int x = 0;x < 16;++x) {
       c = closebtn[y][x];
@@ -118,6 +122,7 @@ void HariMain(void)
   };
 
   unsigned int memtotal;
+  int key_to;
 
 
   init_gdtidt();
@@ -226,6 +231,21 @@ void HariMain(void)
           str_renderer_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
           cursor_x -= 8;
         }
+        if (d == 256 + 0x0f) {
+          //tab
+          if (key_to == 0) {
+            key_to = 1;
+            make_wtitle8(buf_win, sht_win->bxsize, "task_a",  0);
+            make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+          } else {
+            key_to = 0;
+            make_wtitle8(buf_win, sht_win->bxsize, "task_a",  1);
+            make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+          }
+          sheet_refresh(sht_win, 0,0, sht_win->bxsize, 21);
+          sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+        }
+
 
         boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
         sheet_refresh(sht_win, cursor_x, 28, cursor_x+8, 44);
