e68958b5a940ff04a85d766f9ad04d85050bc3f1
gunyagisa <gunyagisa@gmail.com>
2020-04-17 11:38:23 +0900

    harib14e

--- src/bootpack.c
+++ src/bootpack.c
@@ -112,18 +112,31 @@ void HariMain(void)
   struct TASK *task_a, *task_cons;
   struct TIMER *timer;
 
-  static char keytable[0x54] = {
+  static char keytable0[0x80] = {
     0,   0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^',   0,   0,
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[',   0,   0, 'A', 'S',
     'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':',   0,   0, ']', 'Z', 'X', 'C', 'V',
     'B', 'N', 'M', ',', '.', '/',   0, '*',   0, ' ',   0,   0,   0,   0,   0,   0,
     0,   0,   0,   0,   0,   0,   0, '7', '8', '9', '-', '4', '5', '6', '+', '1',
-    '2', '3', '0', '.'
+    '2', '3', '0', '.', 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0x5c, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x5c, 0, 0
   };
 
-  unsigned int memtotal;
-  int key_to;
+  static char keytable1[0x80] = {
+    0,   0, '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~',   0,   0,
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{',   0,   0, 'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*',   0,   0, '}', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', ',', '.', '?',   0, '*',   0, ' ',   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0, '7', '8', '9', '-', '4', '5', '6', '+', '1',
+    '2', '3', '0', '.', 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, '_', 0, 0, 0, 0, 0, 0, 0, 0, 0, '|', 0, 0
+  };
+  int key_shift = 0, key_to = 0;
 
+
+  unsigned int memtotal;
 
   init_gdtidt();
   init_pic();
@@ -219,16 +232,26 @@ void HariMain(void)
       if (256 <= d && d <= 511) { //keyboard
         sprintf(s, "%x", d - 256);
         str_renderer_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-        if (d < 0x54 + 256 && keytable[d - 256] != 0) { //normal chara
-          if (key_to == 0) { // taskA
+        if (d < 0x80 + 256) { //normal chara
+          if (key_shift == 0) {
+            s[0] = keytable0[d - 256];
+          } else {
+            s[0] = keytable1[d - 256];
+          }
+        } else {
+          s[0] = 0;
+        }
+
+        if (s[0] != 0) {
+          if (key_to == 0) {
             if (cursor_x < 128) {
-              s[0] = keytable[d - 256];
+              //task A
               s[1] = 0;
               str_renderer_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
               cursor_x += 8;
             }
           } else {
-            fifo32_put(&task_cons->fifo, keytable[d - 256] + 256);
+            fifo32_put(&task_cons->fifo, s[0] + 256);
           }
         }
         if (d == 256 + 0x0e) {
@@ -255,8 +278,18 @@ void HariMain(void)
           sheet_refresh(sht_win, 0,0, sht_win->bxsize, 21);
           sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
         }
-
-
+        if (d == 256 + 0x2a) {
+          key_shift |= 1;
+        }
+        if (d == 256 + 0x36) {
+          key_shift |= 2;
+        }
+        if (d == 256 + 0xaa) {
+          key_shift &= ~1;
+        }
+        if (d == 256 + 0xb6) {
+          key_shift &= ~2;
+        }
         boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
         sheet_refresh(sht_win, cursor_x, 28, cursor_x+8, 44);
 
