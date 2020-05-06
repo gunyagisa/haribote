434730d307a9a215d37cfb8a458445f90c4c199c
gunyagisa <gunyagisa@gmail.com>
2020-05-06 17:56:43 +0900

    harib23c

--- src/sheet.c
+++ src/sheet.c
@@ -111,7 +111,7 @@ void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
 {
-  int vx, vy;
+  int vx, vy, sid4, *p, *q, *r, i, i1;
   unsigned char *buf, sid, *vram = ctl->vram, *map = ctl->map;
   struct SHEET *sht;
 
@@ -120,7 +120,7 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
   if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
   if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
 
-  for (int h = h0;h <= h1;h++) {
+  for (int h = h0; h <= h1; h++) {
     sht = ctl->sheets[h];
     buf = sht->buf;
     sid = sht - ctl->sheets0;
@@ -134,12 +134,60 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
     if (by0 < 0) { by0 = 0; }
     if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
     if (by1 > sht->bysize) { by1 = sht->bysize; }
-    for (int by = by0;by < by1;by++) {
-      vy = sht->vy0 + by;
-      for (int bx = bx0;bx < bx1;bx++) {
+
+    int bx, bx2;
+    if ((sht->vx0 & 3) == 0) {
+      i = (bx0 + 3) / 4;
+      i1 = bx1 / 4;
+      i1 = i1 - i;
+      sid4 = sid | sid << 8 | sid << 16 | sid << 24;
+      for (int by = by0; by < by1; by++) {
+        vy = sht->vy0 + by;
+        for (bx = bx0; bx < bx1 && (bx & 3) != 0; bx++) {
+          vx = sht->vx0 + bx;
+          if(map [vy * ctl->xsize + vx] == sid) {
+            vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+          }
+        }
         vx = sht->vx0 + bx;
-        if (map[vx + vy * ctl->xsize] == sid) {
-          vram[vx + vy * ctl->xsize] = buf[bx + by * sht->bxsize];
+        p = (int *) &map[vy * ctl->xsize + vx];
+        q = (int *) &vram[vy * ctl->xsize + vx];
+        r = (int *) &buf[by * sht->bxsize + bx];
+        for (int i = 0; i < i1; i++) {
+          if (p[i] == sid4) {
+            q[i] = r[i];
+          } else {
+            bx2 = bx + i * 4;
+            vx = sht->vx0 + bx2;
+            if (map[vy * ctl->xsize + vx + 0] == sid) {
+              vram[vy * ctl->xsize + vx + 0] = buf[by * sht->bxsize + bx2 + 0];
+            }
+            if (map[vy * ctl->xsize + vx + 1] == sid) {
+              vram[vy * ctl->xsize + vx + 1] = buf[by * sht->bxsize + bx2 + 1];
+            }
+            if (map[vy * ctl->xsize + vx + 2] == sid) {
+              vram[vy * ctl->xsize + vx + 2] = buf[by * sht->bxsize + bx2 + 2];
+            }
+            if (map[vy * ctl->xsize + vx + 3] == sid) {
+              vram[vy * ctl->xsize + vx + 3] = buf[by * sht->bxsize + bx2 + 3];
+            }
+          }
+        }
+        for (bx += i1 * 4; bx < bx1; bx++) {
+          vx = sht->vx0 + bx;
+          if (map[vy * ctl->xsize + vx] == sid) {
+            vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+          }
+        }
+      }
+    } else {
+      for (int by = by0; by < by1; by++) {
+        vy = sht->vy0 + by;
+        for (int bx = bx0; bx < bx1; bx++) {
+          vx = sht->vx0 + bx;
+          if (map[vx + vy * ctl->xsize] == sid) {
+            vram[vx + vy * ctl->xsize] = buf[bx + by * sht->bxsize];
+          }
         }
       }
     }
