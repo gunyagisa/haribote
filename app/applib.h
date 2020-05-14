9f44c1ec0fabbf02832b9f1c8bf2532ef93806e0
gunyagisa <gunyagisa@gmail.com>
2020-05-14 15:23:28 +0900

    add mmlplayer app

--- app/applib.h
+++ app/applib.h
@@ -13,7 +13,7 @@ void api_point(int win, int x, int y, int col);
 void api_refreshwin(int win, int x0,int y0, int x1, int y1);
 void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
 int api_getkey(int mode);
-int alloc_timer(void);
+int api_alloctimer(void);
 void api_inittimer(int timer, int data);
 void api_settimer(int timer, int time);
 void api_freetimer(int timer);
